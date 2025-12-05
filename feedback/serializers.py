e948799595bbf0b0448231696fa28c9032ed63f4
ChrisOnsando <chrisnemwel@gmail.com>
2025-12-05 12:49:26 +0300

    ft(teampulse): make changes in the project(#19)

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from feedback.models import TeamFeedback
 from teams.models import Team
 
+
 class TeamFeedbackSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
     username = serializers.SerializerMethodField()
@@ -11,7 +12,7 @@ class TeamFeedbackSerializer(serializers.ModelSerializer):
     team = serializers.UUIDField(write_only=True, required=False, allow_null=True)
     team_name = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(read_only=True)
-    
+
     class Meta:
         model = TeamFeedback
         fields = (
@@ -24,7 +25,7 @@ class TeamFeedbackSerializer(serializers.ModelSerializer):
             "created_at",
         )
         read_only_fields = ("id", "username", "team_name", "created_at")
-    
+
     def get_username(self, obj: Any) -> str:
         """
         Return 'Anonymous' if feedback is anonymous, otherwise return username
@@ -32,7 +33,7 @@ class TeamFeedbackSerializer(serializers.ModelSerializer):
         if obj.is_anonymous:
             return "Anonymous"
         return obj.user.username
-    
+
     def get_team_name(self, obj: Any) -> str:
         """
         Return team name or 'General' if no team
@@ -40,12 +41,11 @@ class TeamFeedbackSerializer(serializers.ModelSerializer):
         if obj.team:
             return obj.team.team_name
         return "General"
-    
+
     def create(self, validated_data: Any) -> Any:
         user = self.context["request"].user
-        
         team_id = validated_data.pop("team", None)
-        
+
         if user.is_staff:
             if team_id:
                 try:
@@ -58,15 +58,25 @@ class TeamFeedbackSerializer(serializers.ModelSerializer):
                 validated_data["team"] = None
         else:
             user_teams = user.teams.all()
-            
             if not user_teams.exists():
                 raise serializers.ValidationError(
                     {"error": "You must belong to a team to submit feedback"}
                 )
             
-            validated_data["team"] = user_teams.first()
-        
+            if team_id:
+                try:
+                    team = Team.objects.get(id=team_id)
+                    if team not in user_teams:
+                        raise serializers.ValidationError(
+                            {"team": "You are not a member of this team"}
+                        )
+                    validated_data["team"] = team
+                except Team.DoesNotExist:
+                    raise serializers.ValidationError(
+                        {"team": "Team not found"}
+                    )
+            else:
+                validated_data["team"] = user_teams.first()
+
         validated_data["user"] = user
-        
         return super().create(validated_data)
-    
